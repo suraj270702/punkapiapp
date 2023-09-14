be3ccec7ca9fb94ab82294ce59f882389a2a904f
suraj270702 <surajmaurya2707@eng.rizvi.edu.in>
2023-09-14 21:24:56 +0530

    code is formatted

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,68 +1,63 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import AppLoader from './Loader'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import AppLoader from "./Loader";
 
-const Card = ({queryInput}) => {
-    const [data,setData] = useState()
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await axios.get('https://api.punkapi.com/v2/beers');
-            setData(response.data); // Assuming the data you want is in response.data
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
-    
-        fetchData();
-        
-      }, [])
+const Card = ({ queryInput }) => {
+  const [data, setData] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://api.punkapi.com/v2/beers");
+        setData(response.data); // Assuming the data you want is in response.data
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-      const filteredData = data?.filter((item) =>
+    fetchData();
+  }, []);
+
+  const filteredData = data?.filter((item) =>
     item.name.toLowerCase().includes(queryInput.toLowerCase())
   );
-      
+
   return (
-   <>
-   
-  
-{
-    !data ? ( <AppLoader />) : (
+    <>
+      {!data ? (
+        <AppLoader />
+      ) : (
         <div>
-  <div className="px-10 py-20  grid gap-10 lg:grid-cols-3 xl:grid-cols-4 sm:grid-cols-2">
-    {
-        (queryInput === '' ? data : filteredData)?.map((item,i)=>(
-            <div className="max-w-xs rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer card">
-      <div>
-      <img
-  src={item?.image_url}
-  className='h-48 object-contain mx-auto'
-  alt=""
-/>
-      </div>
-      <div className="py-4 px-4 bg-white">
-        <h3 className="text-md font-semibold text-gray-600">{item?.name}</h3>
-        <p className="mt-4 text-lg font-thin description">{item?.description.substring(0,80)}...</p>
-        <h3 className="text-md font-semibold text-gray-600">Food Pairings</h3>
-        {
-            item.food_pairing.map((food)=>(
-                <p className="mt-4 text-lg font-thin description">{food}</p>
-            ))
-        }
-        
-        
-      </div>
-    </div>
-        ))
-    }
-    
-  </div>
-</div>
-    )
-}
+          <div className="px-10 py-20  grid gap-10 lg:grid-cols-3 xl:grid-cols-4 sm:grid-cols-2">
+            {(queryInput === "" ? data : filteredData)?.map((item, i) => (
+              <div className="max-w-xs rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer card">
+                <div>
+                  <img
+                    src={item?.image_url}
+                    className="h-48 object-contain mx-auto"
+                    alt=""
+                  />
+                </div>
+                <div className="py-4 px-4 bg-white">
+                  <h3 className="text-md font-semibold text-gray-600">
+                    {item?.name}
+                  </h3>
+                  <p className="mt-4 text-lg font-thin description">
+                    {item?.description.substring(0, 80)}...
+                  </p>
+                  <h3 className="text-md font-semibold text-gray-600">
+                    Food Pairings
+                  </h3>
+                  {item.food_pairing.map((food) => (
+                    <p className="mt-4 text-lg font-thin description">{food}</p>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
-   </>
-  )
-}
-
-export default Card
+export default Card;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,31 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const Search = ({setQueryInput}) => {
-    const handleSearch = (e) => {
-        const query = e.target.value;
-        setQueryInput(query); // Update the search query in the parent component
-      };
-    
+const Search = ({ setQueryInput }) => {
+  const handleSearch = (e) => {
+    const query = e.target.value;
+    setQueryInput(query); // Update the search query in the parent component
+  };
+
   return (
     <>
-    
-    
-<div class="fixed top-0 left-0 w-full h-16   select-none">
-  <div class="w-5/6 z-50 relative mx-auto ">
-    <div class=" w-full h-16 rounded-xl mb-3 shadow-lg p-2">
-      <input type="text" placeholder="Search"  onChange={handleSearch} class="w-full h-full text-2xl rounded-lg focus:outline-none focus:ring focus:border-blue-300" />
-    </div>
-    
-  </div>
-</div>
-
-
-
-
-
-
+      <div class="fixed top-0 left-0 w-full h-16   select-none">
+        <div class="w-5/6 z-50 relative mx-auto ">
+          <div class=" w-full h-16 rounded-xl mb-3 shadow-lg p-2">
+            <input
+              type="text"
+              placeholder="Search"
+              onChange={handleSearch}
+              class="w-full h-full text-2xl rounded-lg focus:outline-none focus:ring focus:border-blue-300"
+            />
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
